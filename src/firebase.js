351d44385f27e98f7bc5e907b8e342d50f202be7
Ritap <deyritap28@gmail.com>
2023-02-03 09:43:06 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBiREwg8eAzk9tMXwwoqI79B6nlQhDuTaw",
+    authDomain: "netflix-clone-6b568.firebaseapp.com",
+    projectId: "netflix-clone-6b568",
+    storageBucket: "netflix-clone-6b568.appspot.com",
+    messagingSenderId: "514362593319",
+    appId: "1:514362593319:web:791e6cc99f99d8b8bb3e02"
+};
